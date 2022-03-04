580897623f2ea9b8c97d493fcc7f236b2772d97f
josxavier1 <josxavier@hotmail.com>
2022-03-04 11:12:12 -0500

    db changes to connect to mysql.

--- rest-api-example/src/main/java/com/jx/restapiexample/domain/SalesCustomer.java
+++ rest-api-example/src/main/java/com/jx/restapiexample/domain/SalesCustomer.java
@@ -3,6 +3,7 @@ package com.jx.restapiexample.domain;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
